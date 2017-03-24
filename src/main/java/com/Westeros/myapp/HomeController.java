53307d50318f8050a29a9477a7a3a3e06f403a80
Matt Gmur <mattgmur@SDMacBookPro2.local>
2017-03-24 11:22:55 -0600

    added controller and home.jsp

--- src/main/java/com/Westeros/myapp/HomeController.java
+++ src/main/java/com/Westeros/myapp/HomeController.java
@@ -6,33 +6,36 @@ import java.util.Locale;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.SessionAttributes;
 
-/**
- * Handles requests for the application home page.
- */
+import data.MLBDAO;
+import data.Team;
+
 @Controller
+@SessionAttributes("obj");
 public class HomeController {
 	
-	private static final Logger logger = LoggerFactory.getLogger(HomeController.class);
-	
-	/**
-	 * Simply selects the home view to render by returning its name.
-	 */
-	@RequestMapping(value = "/", method = RequestMethod.GET)
-	public String home(Locale locale, Model model) {
-		logger.info("Welcome home! The client locale is {}.", locale);
-		
-		Date date = new Date();
-		DateFormat dateFormat = DateFormat.getDateTimeInstance(DateFormat.LONG, DateFormat.LONG, locale);
-		
-		String formattedDate = dateFormat.format(date);
-		
-		model.addAttribute("serverTime", formattedDate );
-		
+	@Autowired
+	private WesterosDAO dao;
+
+//	@ModelAttribute("obj")
+//	public House initHouse() {
+//		return new House();
+//	}
+
+	public void setWesterosDAO(WesterosDAO w) {
+		this.WesterosDAO = w;
+	}
+
+	@RequestMapping(path = "welcome.do", method = RequestMethod.GET)
+	public String getLeague(Model model) {
+		model.addAttribute("houses", dao.getHouse());
 		return "home";
 	}
 	
