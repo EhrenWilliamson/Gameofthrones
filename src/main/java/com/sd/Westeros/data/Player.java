2cac89d0e37a82afd057323453966ca29efbc69a
StefanFuller147 <sfullersywn@gmail.com>
2017-03-24 11:30:09 -0600

    Stefan's inital commit

---
+++ src/main/java/com/sd/Westeros/data/Player.java
@@ -0,0 +1,41 @@
+package com.sd.Westeros.data;
+
+public class Player {
+	private String firstName;
+	private String lastName;
+	private String nickName;
+	private String status;
+	public String getFirstName() {
+		return firstName;
+	}
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+	public String getLastName() {
+		return lastName;
+	}
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+	public String getNickName() {
+		return nickName;
+	}
+	public void setNickName(String nickName) {
+		this.nickName = nickName;
+	}
+	public String getStatus() {
+		return status;
+	}
+	public void setStatus(String status) {
+		this.status = status;
+	}
+	@Override
+	public String toString() {
+		return "Player [firstName=" + firstName + ", lastName=" + lastName + ", nickName=" + nickName + ", status="
+				+ status + "]";
+	}
+	
+	public Player(){
+		
+	}
+}
