18541cb5e3cee3d86a5067027d6edc8bd6ce0c6d
StefanFuller147 <sfullersywn@gmail.com>
2017-03-24 12:53:49 -0600

    stefan's third push

---
+++ src/main/java/com/sd/Westeros/data/House.java
@@ -0,0 +1,33 @@
+package com.sd.Westeros.data;
+
+public class House {
+	private String name;
+	private String location;
+	private String headOfHouse;
+	@Override
+	public String toString() {
+		return "House [name=" + name + ", location=" + location + ", headOfHouse=" + headOfHouse + "]";
+	}
+	public String getHeadOfHouse() {
+		return headOfHouse;
+	}
+	public void setHeadOfHouse(String headOfHouse) {
+		this.headOfHouse = headOfHouse;
+	}
+	
+	public House(){
+		
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getLocation() {
+		return location;
+	}
+	public void setLocation(String location) {
+		this.location = location;
+	}
+}
